662bed93c3cf5b02bc049628cd9e27e72b4f0e30
Armando Miani <armadom@google.com>
2021-07-29 12:54:07 +0200

    Add disk size

--- scripts/packergen.py
+++ scripts/packergen.py
@@ -155,12 +155,11 @@ def main():
       },
       'tags': ['imagebuilder-workers']
   }
-  if data.get('source_image_family'):
-    builder['source_image_family'] = data['source_image_family']
-  if data.get('image_family'):
-    builder['image_family'] = data['image_family']
-  if data.get('disk_size'):
-    builder['disk_size'] = data['disk_size']
+
+  # Patch actual packer file with allowed attributes from packer.in.json
+  for key in ["source_image_family", "image_family", "disk_size"]:
+		if data.get(key):
+			builder[key] = data.get(key)
 
   content = {
       'variables': {
