669acf8ef2d8e84abc8318d1e52ec1923f92187e
gibbleyg <63262871+gibbleyg@users.noreply.github.com>
2022-10-04 10:19:40 -0700

    Use python3 and update to newest version of Packer (#48)
    Use python3 and update Packer

--- scripts/packergen.py
+++ scripts/packergen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2018 Google LLC
 #
@@ -193,7 +193,7 @@ def main():
           _verify_shutdown_script()
       ],
   }
-  print json.dumps(content, sort_keys=True, indent=2)
+  print(json.dumps(content, sort_keys=True, indent=2))
 
   return 0
 
